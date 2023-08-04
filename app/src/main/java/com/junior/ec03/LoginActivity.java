bc364cfcecf797f2f634f0a6efd98d14a7be5f0c
JuniorCarrasco <jujocahu019@gmail.com>
2023-08-04 12:54:20 -0500

    primer commit

---
+++ app/src/main/java/com/junior/ec03/LoginActivity.java
@@ -0,0 +1,76 @@
+package com.junior.ec03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+
+import com.junior.ec03.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding; //BINDING
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater()); //BINDING
+        setContentView(binding.getRoot()); //BINDING
+
+        binding.btnLogin.setOnClickListener(v -> {
+            //Toast.makeText(this,"Datos correctos...", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+        binding.titleEmail.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                boolean isOk = isCredentialsValidate(s.toString(),binding.tilPassword.getEditText().getText().toString());
+                binding.btnLogin.setEnabled(isOk);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        binding.tilPassword.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int before) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                boolean isOk = isCredentialsValidate(binding.titleEmail.getEditText().getText().toString(), s.toString());
+                binding.btnLogin.setEnabled(isOk);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+        });
+
+    }
+
+    private Boolean isCredentialsValidate(String email, String password){
+        boolean isEmailOk = !email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        boolean isPasswordOk = password.length() >= 8;
+
+        return isEmailOk && isPasswordOk;
+
+    }
+}
