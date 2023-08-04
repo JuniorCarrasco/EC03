a9e859681b3fae869864178eff5c43fd5d3c7a69
JuniorCarrasco <jujocahu019@gmail.com>
2023-08-04 14:10:27 -0500

    "Se subio"

--- app/src/main/java/com/junior/ec03/fragments/RVResumenAdapter.java
+++ app/src/main/java/com/junior/ec03/fragments/RVResumenAdapter.java
@@ -1,13 +1,22 @@
 package com.junior.ec03.fragments;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RVResumenAdapter extends RecyclerView.Adapter <RVResumenAdapter.ResumeVH> {
+import com.junior.ec03.modelo.PlatoModel;
 
+import java.util.List;
+
+public class RVResumenAdapter extends RecyclerView.Adapter <RVResumenAdapter.ResumeVH> {
+    private List<PlatoModel> Plato;
+
+    public RVResumenAdapter(List<PlatoModel> plato) {
+        this.Plato = plato;
+    }
 
     @NonNull
     @Override
@@ -18,11 +27,14 @@ public class RVResumenAdapter extends RecyclerView.Adapter <RVResumenAdapter.Res
     @Override
     public void onBindViewHolder(@NonNull ResumeVH holder, int position) {
 
+
+
+        return null;
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return Plato.size();
     }
 
     class ResumeVH extends RecyclerView.ViewHolder{
---
+++ app/src/main/java/com/junior/ec03/modelo/PlatoModel.java
@@ -0,0 +1,27 @@
+package com.junior.ec03.modelo;
+
+public class PlatoModel {
+    private String name;
+    private String imgUrl;
+
+    public PlatoModel(String name, String imgUrl) {
+        this.name = name;
+        this.imgUrl = imgUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
